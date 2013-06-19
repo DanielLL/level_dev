16fe5d7815fd15aabce1619f1bc2230451ffc92b
DanielLL <daniel.lopez@tangosource.com>
2013-06-19 10:50:29 -0500

    Initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+LevelDev::Application.config.secret_token = 'e48b1411f8e6a27627a845b1836e4c9c0c0081992e7efe279420bdbade16d582bf748762d8aab4a4ccbebc1f591fcb58b0b10dadc9b8c2cab37aad496aeab987'
