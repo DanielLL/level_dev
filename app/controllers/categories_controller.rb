08ea4a957c92e43c26304e0abad6f12dbc4ec041
Juan Carlos <juancarlos@tangosource.com>
2013-06-26 17:36:22 -0500

    fixed error in create method

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,37 +1,44 @@
 class CategoriesController < ApplicationController
+  expose(:categories)
+  expose(:category, attributes: :category_attributes)
 
   def index
-    @categories = Category.all
   end
 
   def create
-    @category = Category.new(params[:category].permit(:name, :description, :percentage))
-    if @category.save
-      redirect_to action: :show, id: @category.id
+    category = Category.new(category_attributes)
+    if category.save
+      redirect_to category_path(category)
     else
       render :new
     end
   end
 
   def new
-    @category = Category.new
   end
 
   def show
-    @category = Category.find(params[:id])
   end
 
   def edit
-    @category = Category.find(params[:id])
   end
 
   def update
-    @category = Category.find(params[:id])
-    if @category.update(params[:category].permit(:name))
-      redirect_to action: :show, id: @category.id
+    if category.save
+      redirect_to category_path(category)
     else
       render :edit
     end
+  end
+
+  private 
+
+  def category_attributes
+    params.require(:category).permit(
+      :name,
+      :description,
+      :percentage
+    )
   end
 
 end
