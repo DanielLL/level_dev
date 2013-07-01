26c1db2adfefcacaa861668360d371fee03e209a
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-07-01 09:40:42 -0500

    Fixed issue at routes file and in the show template

--- config/routes.rb
+++ config/routes.rb
@@ -59,5 +59,4 @@ LevelDev::Application.routes.draw do
   # This is a legacy wild controller route that's not recommended for RESTful applications.
   # Note: This route will make all actions in every controller accessible via GET requests.
   # match ':controller(/:action(/:id))(.:format)'
->>>>>>> compare if is admin show all developer else only the  name of developer
 end
