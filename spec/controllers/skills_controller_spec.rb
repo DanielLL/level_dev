bbfaa1cc3d2744d4ffd35eb75a32e5c9c01e675c
Juan Carlos <juancarlos@tangosource.com>
2013-07-03 11:24:05 -0500

    added some tests

--- spec/controllers/skills_controller_spec.rb
+++ spec/controllers/skills_controller_spec.rb
@@ -2,6 +2,30 @@ require 'spec_helper'
 
 describe SkillsController do
 
-  pending 'Pending'
+  before(:each) do
+    @category = FactoryGirl.create(:category)
+  end
+
+  context 'index' do
+    it 'should get 200 code' do
+      get :index, category_id: @category.id
+      response.should be_success
+    end
+  end
+
+  context 'new' do
+    it 'should get 200 code' do
+      get :new, category_id: @category.id
+      response.should be_success
+    end
+  end
+
+  context 'edit' do
+    skill = FactoryGirl.create(:skill)
+    it 'should get 200 code' do
+      get :edit, category_id: @category.id, id: skill.id
+      response.should be_success
+    end
+  end
 
 end
