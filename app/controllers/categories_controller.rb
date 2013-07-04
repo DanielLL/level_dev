1a73eb0f9a2c916ecd8487347a21e121309fc882
DanielLL <daniel.lopez@tangosource.com>
2013-07-04 15:34:07 -0500

    Refactor of Categories controller

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -3,60 +3,49 @@ class CategoriesController < ApplicationController
   expose(:categories)
   expose(:category, attributes: :category_attributes)
 
+  before_filter :is_admin?, except: [:index, :show]
+
   def index
   end
 
   def create
-    if current_developer.admin
       if category.save
         redirect_to category_path(category)
       else
         render :new
       end
-    else
-      redirect_to categories_path
-    end
   end
 
   def new
-    if current_developer.admin
       render :new
-    else
-      redirect_to categories_path
-    end
   end
 
   def show
   end
 
   def edit
-    if current_developer.admin
       render :new
-    else
-      redirect_to categories_path
-    end
   end
 
   def update
-    if current_developer.admin
       if category.save
         redirect_to category_path(category)
       else
         render :edit
       end
-    else
-      redirect_to categories_path
-    end
   end
 
   def destroy
-    if current_developer.admin
       if category.destroy
         redirect_to categories_path
       else
         render :index
       end
-    else
+  end
+
+
+  def is_admin?
+    unless current_developer.admin?
       redirect_to categories_path
     end
   end
