20ea79bf1e18981b0ab843811b50fba9f9d6eedc
DanielLL <daniel.lopez@tangosource.com>
2013-07-05 15:40:10 -0500

    Added methods to Developer model to get skills with the visible attribute and modified the jbuilder template and the developer show view

--- app/models/developer.rb
+++ app/models/developer.rb
@@ -7,4 +7,18 @@ class Developer < ActiveRecord::Base
   validates_presence_of :name
 
   has_many :dev_skills, dependent: :destroy
+
+
+  def current_skills(skills)
+    array = []
+    skills.each do |skill|
+      array << [skill.attributes.merge("visible" => self.has_skill?(skill))]
+    end
+    return array
+  end
+
+  def has_skill?(skill)
+    self.dev_skills.where(skill_id: skill.id).present?
+  end
+
 end
--- app/views/categories/index.json.jbuilder
+++ app/views/categories/index.json.jbuilder
@@ -1,5 +1,5 @@
 json.array! categories.sort_by do |category|
-  json.id     category.id
-  json.name   category.name
-  json.skills category.skills.sort_by &:id
+  json.id      category.id
+  json.name    category.name
+  json.skills  current_developer.current_skills(category.skills).flatten!.reverse()
 end
