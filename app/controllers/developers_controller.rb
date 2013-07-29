6368eb75d123e0732ea77485e8a3b0234ee95656
Alejandra Gutierrez <miss_amisa@hotmail.com>
2013-07-29 14:12:43 -0500

    Create edit view of developers

--- app/controllers/developers_controller.rb
+++ app/controllers/developers_controller.rb
@@ -3,7 +3,7 @@ class DevelopersController < ApplicationController
   before_filter :is_admin?
 
   expose(:developers)
-  expose(:developer)
+  expose(:developer, attributes: :developer_attributes)
 
   def index
     if developer_signed_in?
@@ -23,6 +23,29 @@ class DevelopersController < ApplicationController
     if current_developer != (developer rescue nil) && !current_developer.admin?       
       redirect_to developer_path(current_developer)
     end
+  end
+
+  def edit
+  end
+
+  def update
+    save_update(:edit)
+  end
+
+  def save_update(action)
+    if developer.save
+      redirect_to developer_path
+    else
+      render action
+    end
+  end
+    private 
+
+  def developer_attributes
+    params.require(:developer).permit(
+      :name,
+      :email
+    )
   end
 
 end
